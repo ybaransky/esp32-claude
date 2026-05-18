130dc381e9226475e3971ab3fed2087812a11ebf
ybaransky <ybaransky@gmail.com>
2026-05-17 22:24:54 -0400

    added the builtin led to blink with a button click

--- src/button.cpp
+++ src/button.cpp
@@ -2,11 +2,17 @@
 #include <OneButton.h>  // mathertel/OneButton
 #include <Arduino.h>
 
+constexpr uint8_t BUTTON_PIN = 23;
+constexpr uint8_t LED_PIN    = 2;   // ESP32 dev board built-in LED
+
 // Pin 23, active-low, internal pull-up enabled.
-static OneButton btn(23, true, true);
+static OneButton btn(BUTTON_PIN, true, true);
 
 static void onClick() {
     Serial.println("[BTN] Single press detected");
+    digitalWrite(LED_PIN, HIGH);
+    delay(200);
+    digitalWrite(LED_PIN, LOW);
 }
 
 static void onDoubleClick() {
@@ -14,6 +20,7 @@ static void onDoubleClick() {
 }
 
 void buttonSetup() {
+    pinMode(LED_PIN, OUTPUT);
     btn.attachClick(onClick);
     btn.attachDoubleClick(onDoubleClick);
 }
